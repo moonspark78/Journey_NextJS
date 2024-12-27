e51b5a6e53806716a1c98cfb8213484059eee86f
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-12-27 17:38:48 +0100

    done with  the navbar  :art:

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -1,8 +1,32 @@
+import { navLinks } from '@/constant/constant'
+import Link from 'next/link'
 import React from 'react'
+import { CgClose } from 'react-icons/cg'
 
-const MobileNav = () => {
+type Props = {
+    showNav: boolean;
+    closeNav: ()=>void;
+}
+
+const MobileNav = ({ showNav, closeNav }: Props) => {
+    const navOpen = showNav ? 'translate-x-0' : 'translate-x-[-100%]';
   return (
-    <div>MobileNav</div>
+    <div>
+        <div className={`${navOpen} text-white fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 w-[80%]
+         sm:w-[60%] bg-indigo-900 space-y-6 z-[100006]`}>
+            {/* NavLinks */}
+            {navLinks.map((link)=> {
+                return (
+                    <Link key={link.id} href={link.url}>
+                        <p className='nav__links text-[20px] ml-12 border-b-[1.5px] pb-2 border-white sm:text-[30px]'>
+                            {link.label}
+                        </p>
+                    </Link>
+                )
+            })}
+            <CgClose onClick={closeNav}   className='absolute top-2 right-10 sm:w-8 sm:h-8 w-6 h-6 text-white cursor-pointer'/>
+        </div>
+    </div>
   )
 }
 
--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -3,17 +3,41 @@
 import { navLinks } from '@/constant/constant'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { HiBars3BottomRight } from 'react-icons/hi2'
 
-const Nav = () => {
+
+
+
+type Props={
+    openNav: ()=>void;
+}
+
+const Nav = ({openNav}:Props) => {
+    const [navBg, setNavBg] = useState(false);
+
+    useEffect(() => {
+        const handler = () => {
+            if(window.scrollY >= 90) {
+                setNavBg(true);
+            } 
+            if(window.scrollY < 90) {
+                setNavBg(false);
+            }
+        };
+        window.addEventListener('scroll', handler);
+        return () => window.removeEventListener('scroll', handler);
+    }, [])
+
+
+
   return (
-    <div className='fixed w-full transition-all duration-200 h-[12vh] z-[1000] bg-blue-700'>
-        <div className='flex justify-between items-center h-full w-[90%] lg:w-[80%] mx-auto'>
+    <div className={`fixed ${navBg ? "bg-indigo-800" : "fixed"} w-full transition-all duration-200 h-[12vh] z-[1000] bg-blue-700`}>
+        <div className='flex justify-between items-center h-full w-[90%] xl:w-[80%] mx-auto'>
             {/* Logo */}
             <Image src={"/images/logo.png"} alt='logo' width={120} height={120}/>
             {/* NavLinls  */}
-            <div className='hidden lg:flex items-center space-x-10'>
+            <div className='hidden lg:flex items-center space-x-8'>
                {navLinks.map((link)=> {
                     return (
                         <Link key={link.id} href={link.url}>
@@ -24,12 +48,12 @@ const Nav = () => {
             </div>
             {/* Buttons */}
             <div className='flex items-center space-x-5'>
-                <button className='md:px-10 md:py-2 py-1.5 text-white font-semibold text-base bg-pink-700 hover:bg-pink-900
+                <button className='md:px-10 md:py-2 px-8 py-1.5 text-white font-semibold text-base bg-pink-700 hover:bg-pink-900
                 transition-all duration-200 rounded-lg'>
                     Signup Now
                 </button>
                 {/* Burger Menu */}
-                <HiBars3BottomRight className='w-8 h-8 cursor-pointer text-white lg:hidden'/>
+                <HiBars3BottomRight onClick={openNav} className='w-8 h-8 cursor-pointer text-white lg:hidden'/>
             </div>
         </div>
     </div>
