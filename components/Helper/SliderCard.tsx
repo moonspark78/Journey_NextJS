80db3073838079da11d5f8bb2226357d350364ed
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-12-31 09:36:22 +0100

    done slider section

--- components/Helper/SliderCard.tsx
+++ components/Helper/SliderCard.tsx
@@ -18,22 +18,27 @@ const SliderCard = ({image, name, role }:Props) => {
                 alt="image"
                 width={350}
                 height={350}
-                className='w-full h-full object-cover'
+                className='w-full h-full'
             />
         </div>
         <div className='mt-6 mb-7'>
             <div className='flex items-center'>
-                <FaStar className='lg:w-7 lg:h-7 md:w-5 md:h-5 w-4 h-4 text-yellow-600'/>
-                <FaStar className='lg:w-7 lg:h-7 md:w-5 md:h-5 w-4 h-4 text-yellow-600'/>
-                <FaStar className='lg:w-7 lg:h-7 md:w-5 md:h-5 w-4 h-4 text-yellow-600'/>
-                <FaStar className='lg:w-7 lg:h-7 md:w-5 md:h-5 w-4 h-4 text-yellow-600'/>
-                <FaStar className='lg:w-7 lg:h-7 md:w-5 md:h-5 w-4 h-4 text-yellow-600'/>
+                <FaStar className='xl:w-7 xl:h-7 md:w-5 md:h-5 w-4 h-4 text-yellow-600'/>
+                <FaStar className='xl:w-7 xl:h-7 md:w-5 md:h-5 w-4 h-4 text-yellow-600'/>
+                <FaStar className='xl:w-7 xl:h-7 md:w-5 md:h-5 w-4 h-4 text-yellow-600'/>
+                <FaStar className='xl:w-7 xl:h-7 md:w-5 md:h-5 w-4 h-4 text-yellow-600'/>
+                <FaStar className='xl:w-7 xl:h-7 md:w-5 md:h-5 w-4 h-4 text-yellow-600'/>
+                
             </div>
             <p className='mt-6 text-gray-800 w-[95%] md:w-[80%] text-xs md:text-sm lg:text-base font-semibold text-opacity-65'> 
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto sapiente harum error quo,
                 doloribus voluptatibus impedit rem laboriosam, obcaecati, aliquam veniam voluptatem modi perspiciatis Neque.
             </p>
-        </div>
+            <div className='mt-7'>
+                <h1 className='text-xl text-black font-semibold'>{name}</h1>
+                <p className='text-black text-lg opacity-60'>{role}</p>
+            </div>
+        </div> 
     </div>
   )
 }
